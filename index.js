25e71207e4422f1e6fc6da198d6a2616a5e84800
Brendon BOUZAS <Brendon.Bouzas@gmail.com>
2018-11-20 19:37:59 +0900

    added express server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 const morgan = require("morgan");
 
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./dist")));
 
 app.listen(process.env.PORT || 4000, () =>
   console.log(`Listening on port ${4000}`)
