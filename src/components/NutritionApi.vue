347401670f21f7e2c9ff7acc417997cd7b9540fd
mp40 <philps.matthew@gmail.com>
2018-11-21 10:33:09 +0900

    nothing important

---
+++ src/components/NutritionApi.vue
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+// unirest.get("https://nutritionix-api.p.rapidapi.com/v1_1/item?upc=49000036756")
+//     .header("X-Mashape-Key", "krltmsPgUfmsh8D3xNg5RFh035Unp19nowUjsnO22I54TbGKxj")
+//     .header("X-Mashape-Host", "nutritionix-api.p.rapidapi.com")
+//     .end(function (result) {
+//         console.log(result.status, result.headers, result.body);
+//     });
+
+// rapid.call('PackageName', 'FunctionName', {
+//     'ParameterKey1': 'ParameterValue1',
+//     'ParameterKey2': 'ParameterValue2'
+// }).on('success', (payload) => {
+//     /*YOUR CODE GOES HERE*/
+// }).on('error', (payload) => {
+//     /*YOUR CODE GOES HERE*/
+// });
+
+<
+script > < /script>
+export default {
+    name: "NutritionApi",
+} < /script>
